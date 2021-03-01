105795bd072fff6cc66bd1a75a6ad2401b599592
Daniel Kurien <danielmathewkurien@gmail.com>
2021-03-01 14:46:34 -0500

    Revert "Revert "CRUD Functionality For Todos Fixes #3""
    This reverts commit 9414480b91ec4fd4e1ff114de420817801ba1fe4.

--- productivity-pro/src/routeComponents/Home.js
+++ productivity-pro/src/routeComponents/Home.js
@@ -67,6 +67,31 @@ const Home = () => {
     }
   };
 
+  const updateTodo = async (todoId, index) => {
+    console.log(index);
+    let todo = todos.filter((todo) => todo.id === todoId);
+    todo = todo[0];
+
+    const update = prompt("Update todo text or date: (type text or date)");
+    if (update === "text") {
+      const todoText = prompt(
+        "What would you like to update text with",
+        todo.value
+      );
+      todo.value = todoText;
+      const newTodos = todos;
+      newTodos[index] = todo;
+      setTodos([...newTodos]);
+      try {
+        await db.collection("users").doc(currentUser.uid).update({
+          todos: newTodos,
+        });
+      } catch (err) {
+        alert("Todo was not updated. Please refresh page and try again");
+      }
+    }
+  };
+
   return (
     <>
       <h1>Home</h1>
@@ -91,6 +116,13 @@ const Home = () => {
             }}
           >
             Delete Todo
+          </button>
+          <button
+            onClick={() => {
+              updateTodo(todo.id, todos.indexOf(todo));
+            }}
+          >
+            Update Todo
           </button>
         </div>
       ))}
--- productivity-pro/src/services/firebase.js
+++ productivity-pro/src/services/firebase.js
@@ -3,7 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 // Configuration details for firebase were here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAC9jkptAFl1GHyxfz4Efk00JEVPp_KN9I",
+  authDomain: "productivity-pro-d6a0f.firebaseapp.com",
+  projectId: "productivity-pro-d6a0f",
+  storageBucket: "productivity-pro-d6a0f.appspot.com",
+  messagingSenderId: "345487907256",
+  appId: "1:345487907256:web:f477a5cce0c731f182174d",
+};
 //Initializing Firebase app
 firebase.initializeApp(firebaseConfig);
 
