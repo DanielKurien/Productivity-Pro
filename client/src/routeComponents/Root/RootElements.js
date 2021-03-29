769aeb8cf5a4327ebdb6e0ddc9e68bbd4c9a7880
Daniel Kurien <danielmathewkurien@gmail.com>
2021-03-29 14:30:43 -0400

    Completed Home Page and Made it Responsive

--- client/src/routeComponents/Root/RootElements.js
+++ client/src/routeComponents/Root/RootElements.js
@@ -4,3 +4,46 @@ export const RootWrapper = styled.div`
   background-color: #22272e;
   height: 100vh;
 `;
+
+export const RootMain = styled.div`
+  display: flex;
+  height: 88vh;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const RootHeading = styled.h1`
+  font-family: "Noto Sans JP", sans-serif;
+  color: #cdd9e5;
+  font-size: 4rem;
+  font-weight: bold;
+
+  @media screen and (max-width: 600px) {
+    font-size: 2.5rem;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`;
+export const RootImage = styled.img`
+  width: 50%;
+  height: 50%;
+`;
+
+export const TypewriterHeading = styled.h1`
+  display: inline-block;
+  font-family: "Noto Sans JP";
+  color: #5cdb95;
+  text-align: center;
+  font-size: 3rem;
+  @media screen and (max-width: 600px) {
+    font-size: 2rem;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 1.5rem;
+  }
+  @media screen and (max-width: 320px) {
+    font-size: 1.1rem;
+  }
+`;
