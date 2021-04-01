70b2698a1185dd963ecf02f4a13147ac247f39c2
Daniel Kurien <danielmathewkurien@gmail.com>
2021-04-01 09:54:09 -0400

    Fixed additional package-lock.json issue

--- client/src/components/SpotifyWorkPlay/SpotifyWorkPlay.js
+++ client/src/components/SpotifyWorkPlay/SpotifyWorkPlay.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { SpotifyWorkContext } from "../../context/SpotifyWorkContext";
 import SpotifyWebPlayer from "react-spotify-web-playback";
+
 const SpotifyWorkPlay = ({ accessToken }) => {
   const { workPlaylist, workPlaylistSongs } = useContext(SpotifyWorkContext);
   console.log(workPlaylistSongs);
+  console.log(accessToken);
   return (
     <div>
       <h1>Hello</h1>
@@ -11,7 +13,6 @@ const SpotifyWorkPlay = ({ accessToken }) => {
         token={accessToken}
         uris={["spotify:artist:6HQYnRM4OzToCYPpVBInuU"]}
       />
-      ;
     </div>
   );
 };
