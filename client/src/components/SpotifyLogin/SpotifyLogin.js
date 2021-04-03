88e7462ae46772c8809ea71649a99b5b8b76343f
Daniel Kurien <danielmathewkurien@gmail.com>
2021-04-03 07:54:21 -0400

    Revert "Update SpotifyLogin.js"
    This reverts commit b95c0b6c2635c06814669de0cc4a4419a3d2c6a2.

--- client/src/components/SpotifyLogin/SpotifyLogin.js
+++ client/src/components/SpotifyLogin/SpotifyLogin.js
@@ -2,7 +2,8 @@ import React from "react";
 import { SpotifyLink, SpotifyLinkWrapper } from "./SpotifyLoginElements";
 
 const SpotifyLogin = () => {
-  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=7d4b5009c91a4020b5a6b49f2e96d8a7&response_type=code&redirect_uri=https://productivitypro.netlify.app/home&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify%20playlist-modify-private`;
+  const { REACT_APP_SPOTIFY_CLIENT_ID } = process.env;
+  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=https://productivitypro.netlify.app/home&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify%20playlist-modify-private`;
 
   return (
     <SpotifyLinkWrapper>
